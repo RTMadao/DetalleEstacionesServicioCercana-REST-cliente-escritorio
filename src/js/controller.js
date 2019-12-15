393f09ce6d6621de6e3ea2792cf19174968c1da7
Carlos Bello <bellocarlos2810@gmail.com>
2019-12-15 17:04:30 -0500

    peticiones al servidor realizadas

---
+++ src/js/controller.js
@@ -0,0 +1,39 @@
+var map;
+let listaMarcadores = [];
+var list;
+
+
+
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: 10.396108, lng: -75.493935},
+        zoom: 15,
+        disableDefaultUI: true 
+    });
+}
+
+var app = new Vue({
+    el: '#app',
+    data: {
+      estaciones: ''
+    },
+    created: function(){
+        fetch('http://192.168.0.37:3000/station')
+        .then(res => res.json())
+        .then(listaEstaciones => {
+            this.estaciones = listaEstaciones;
+            listaEstaciones.forEach(estacion => {
+                var marker = new google.maps.Marker({
+                    position: {lat: estacion.lat, lng: estacion.lng},
+                    animation: google.maps.Animation.DROP,
+                    title: estacion.nombre
+                });
+                listaMarcadores.push(marker)
+            });
+
+            listaMarcadores.forEach(marcador => {
+                marcador.setMap(map);
+            })
+        })
+    }
+})
